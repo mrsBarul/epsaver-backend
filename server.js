77110f3aabb632018e11403f9b8f51ae2b6cb34f
mrsBarul <dobro.ve@mail.ru>
2024-01-18 13:20:58 +0300

    update auth

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = 8000 || process.env.port;
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL
+    origin: '*'
 }));
 app.use(cookieParser());
 app.use(routes);
